c1a0c387742a4b3d714c6fbde7f7abd87ec14ff5
wahmd <wahmdgit@gmail.com>
2023-02-28 22:34:12 +0500

    :sparkles: feat: created login checks

---
+++ bloggy/src/app/home/posts/posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bloggy-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent {
+
+}
