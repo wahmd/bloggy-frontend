bce2011410f5a3fab1598ff2511a532979bbad29
wahmd <wahmdgit@gmail.com>
2023-02-28 20:09:33 +0500

    :package: setup bootstrap

--- bloggy/src/app/app.component.ts
+++ bloggy/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bloggy';
+  constructor(private modalService: NgbModal) {}
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
 }
