0a9367703e323d0236519ce7d3185708be3c9022
wahmd <wahmdgit@gmail.com>
2023-02-28 20:18:17 +0500

    :sparkles: feat: created empty header component

--- bloggy/src/app/app.module.ts
+++ bloggy/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
